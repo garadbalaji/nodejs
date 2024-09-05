1c94f713b4780542f94e59f05cea0e53b328486d
garadbalaji <balajigarad2017@gmail.com>
2024-09-05 02:38:09 +0000

    welcom

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
  
 // Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('wellcome, World!');
+  res.send('wellcome, pune');
 });
  
 // Define the port number and start the server
